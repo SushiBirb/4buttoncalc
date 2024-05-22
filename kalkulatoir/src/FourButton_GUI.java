633bf21c734a7b28472fb6668ccaf290ce76461e
Sushi <99062833+SushiBirb@users.noreply.github.com>
2024-05-22 13:57:51 -0400

    Update FourButton_GUI.java
    Now allows dividing by a negative and decimal
    
    Removed (-) button for negatives as it was useless
    
    general code cleanup

--- kalkulatoir/src/FourButton_GUI.java
+++ kalkulatoir/src/FourButton_GUI.java
@@ -4,223 +4,238 @@ import java.util.Arrays;
 
 public class FourButton_GUI {
 
-    private double result;
+  private double result;
 
-    public FourButton_GUI() {
-        JFrame frame = new JFrame("Four Button GUI");
+  public FourButton_GUI() {
+    JFrame frame = new JFrame("Four Button GUI");
 
+    //text field
+    JTextField tf = new JTextField(0);
+    tf.setBounds(5, 5, 300, 200);
+    tf.setHorizontalAlignment(SwingConstants.CENTER);
+    tf.setEditable(false);
+    frame.add(tf);
 
-        //text field
-        JTextField tf = new JTextField(0);
-        tf.setBounds(5, 5, 300, 200);
-        tf.setHorizontalAlignment(SwingConstants.CENTER);
-        tf.setEditable(false);
-        frame.add(tf);
+    //buttons
+    JButton b0 = new JButton("0");
+    b0.setBounds(80, 410, 75, 50);
+    frame.add(b0);
 
-        //buttons
-        JButton b0 = new JButton("0");
-        b0.setBounds(80, 410, 75, 50);
-        frame.add(b0);
+    JButton b1 = new JButton("1");
+    b1.setBounds(5, 360, 75, 50);
+    frame.add(b1);
 
-        JButton b1 = new JButton("1");
-        b1.setBounds(5, 360, 75, 50);
-        frame.add(b1);
+    JButton b2 = new JButton("2");
+    b2.setBounds(80, 360, 75, 50);
+    frame.add(b2);
 
-        JButton b2 = new JButton("2");
-        b2.setBounds(80, 360, 75, 50);
-        frame.add(b2);
+    JButton b3 = new JButton("3");
+    b3.setBounds(155, 360, 75, 50);
+    frame.add(b3);
 
-        JButton b3 = new JButton("3");
-        b3.setBounds(155, 360, 75, 50);
-        frame.add(b3);
+    JButton b4 = new JButton("4");
+    b4.setBounds(5, 310, 75, 50);
+    frame.add(b4);
 
-        JButton b4 = new JButton("4");
-        b4.setBounds(5, 310, 75, 50);
-        frame.add(b4);
+    JButton b5 = new JButton("5");
+    b5.setBounds(80, 310, 75, 50);
+    frame.add(b5);
 
-        JButton b5 = new JButton("5");
-        b5.setBounds(80, 310, 75, 50);
-        frame.add(b5);
+    JButton b6 = new JButton("6");
+    b6.setBounds(155, 310, 75, 50);
+    frame.add(b6);
 
-        JButton b6 = new JButton("6");
-        b6.setBounds(155, 310, 75, 50);
-        frame.add(b6);
+    JButton b7 = new JButton("7");
+    b7.setBounds(5, 260, 75, 50);
+    frame.add(b7);
 
-        JButton b7 = new JButton("7");
-        b7.setBounds(5, 260, 75, 50);
-        frame.add(b7);
+    JButton b8 = new JButton("8");
+    b8.setBounds(80, 260, 75, 50);
+    frame.add(b8);
 
-        JButton b8 = new JButton("8");
-        b8.setBounds(80, 260, 75, 50);
-        frame.add(b8);
+    JButton b9 = new JButton("9");
+    b9.setBounds(155, 260, 75, 50);
+    frame.add(b9);
 
-        JButton b9 = new JButton("9");
-        b9.setBounds(155, 260, 75, 50);
-        frame.add(b9);
+    JButton bdot = new JButton(".");
+    bdot.setBounds(155, 410, 75, 50);
+    frame.add(bdot);
 
-        JButton bdot = new JButton(".");
-        bdot.setBounds(155, 410, 75, 50);
-        frame.add(bdot);
+    JButton bClear = new JButton("C");
+    bClear.setBounds(5, 210, 110, 50);
+    frame.add(bClear);
 
-        JButton bClear = new JButton("C");
-        bClear.setBounds(5, 210, 110, 50);
-        frame.add(bClear);
+    JButton bAdd = new JButton("+");
+    bAdd.setBounds(230, 360, 75, 50);
+    frame.add(bAdd);
 
-        JButton bAdd = new JButton("+");
-        bAdd.setBounds(230, 360, 75, 50);
-        frame.add(bAdd);
+    JButton bSub = new JButton("-");
+    bSub.setBounds(230, 310, 75, 50);
+    frame.add(bSub);
 
-        JButton bSub = new JButton("-");
-        bSub.setBounds(230, 310, 75, 50);
-        frame.add(bSub);
+    JButton bMul = new JButton("*");
+    bMul.setBounds(230, 260, 75, 50);
+    frame.add(bMul);
 
-        JButton bMul = new JButton("*");
-        bMul.setBounds(230, 260, 75, 50);
-        frame.add(bMul);
+    JButton bDiv = new JButton("/");
+    bDiv.setBounds(230, 210, 75, 50);
+    frame.add(bDiv);
 
-        JButton bDiv = new JButton("/");
-        bDiv.setBounds(230, 210, 75, 50);
-        frame.add(bDiv);
+    JButton beq = new JButton("=");
+    beq.setBounds(230, 410, 75, 50);
+    frame.add(beq);
 
-        JButton ans = new JButton("ans");
-        ans.setBounds(5, 410, 75, 50);
-        frame.add(ans);
+    JButton del = new JButton("del");
+    del.setBounds(115, 210, 115, 50);
+    frame.add(del);
 
-        JButton beq = new JButton("=");
-        beq.setBounds(230, 410, 75, 50);
-        frame.add(beq);
+    //del.setFont(del.getFont().deriveFont(20.0f));
 
-        JButton del = new JButton("del");
-        del.setBounds(115, 210, 115, 50);
-        frame.add(del);
+    //general frame settings
+    frame.setResizable(false);
+    frame.setSize(325, 505);
+    frame.setLayout(null);
+    frame.setVisible(true);
+    frame.setAlwaysOnTop(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //del.setFont(del.getFont().deriveFont(20.0f));
+    //number buttons add to text field
 
-        //general frame settings
-        frame.setResizable(false);
-        frame.setSize(325, 505);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setAlwaysOnTop(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    b0.addActionListener(e -> tf.setText(tf.getText() + "0"));
 
-        //number buttons add to text field
+    b1.addActionListener(e -> tf.setText(tf.getText() + "1"));
 
+    b2.addActionListener(e -> tf.setText(tf.getText() + "2"));
 
-        b0.addActionListener(e -> tf.setText(tf.getText() + "0"));
+    b3.addActionListener(e -> tf.setText(tf.getText() + "3"));
 
-        b1.addActionListener(e -> tf.setText(tf.getText() + "1"));
+    b4.addActionListener(e -> tf.setText(tf.getText() + "4"));
 
-        b2.addActionListener(e -> tf.setText(tf.getText() + "2"));
+    b5.addActionListener(e -> tf.setText(tf.getText() + "5"));
 
-        b3.addActionListener(e -> tf.setText(tf.getText() + "3"));
+    b6.addActionListener(e -> tf.setText(tf.getText() + "6"));
 
-        b4.addActionListener(e -> tf.setText(tf.getText() + "4"));
+    b7.addActionListener(e -> tf.setText(tf.getText() + "7"));
 
-        b5.addActionListener(e -> tf.setText(tf.getText() + "5"));
+    b8.addActionListener(e -> tf.setText(tf.getText() + "8"));
 
-        b6.addActionListener(e -> tf.setText(tf.getText() + "6"));
+    b9.addActionListener(e -> tf.setText(tf.getText() + "9"));
 
-        b7.addActionListener(e -> tf.setText(tf.getText() + "7"));
+    bdot.addActionListener(e -> tf.setText(tf.getText() + "."));
 
-        b8.addActionListener(e -> tf.setText(tf.getText() + "8"));
+    bAdd.addActionListener(e -> tf.setText(tf.getText() + " + "));
 
-        b9.addActionListener(e -> tf.setText(tf.getText() + "9"));
+    bSub.addActionListener(e -> tf.setText(tf.getText() + " - "));
 
+    bMul.addActionListener(e -> tf.setText(tf.getText() + " * "));
 
-        ans.addActionListener(e -> tf.setText(String.valueOf(result)));
+    bDiv.addActionListener(e -> tf.setText(tf.getText() + " / "));
 
-        bdot.addActionListener(e -> tf.setText(tf.getText() + "."));
+    bClear.addActionListener(e -> tf.setText(""));
 
-        bAdd.addActionListener(e -> tf.setText(tf.getText() + " + "));
+    del.addActionListener(e -> tf.setText(tf.getText().substring(0, tf.getText().length() - 1)));
 
-        bSub.addActionListener(e -> tf.setText(tf.getText() + " - "));
+    //parse text from field
+    beq.addActionListener(e -> {
+      String text = tf.getText();
+      String[] splitequation = text.split(" ");
+      if (splitequation[1].equals("-")) {
+        splitequation[0] = "0";
+      }
+      ArrayList < String > equation = new ArrayList < > (Arrays.asList(splitequation));
+      for (int c = 0; c < equation.size(); c++) {
+        if (equation.get(c).isEmpty()) {
+          equation.remove(c);
+          System.out.println(equation);
+        }
+      }
 
-        bMul.addActionListener(e -> tf.setText(tf.getText() + " * "));
+      //check for 2 operators in a row
+      for (int i = 0; i < equation.size(); i++) {
+        try {
+          if (equation.get(i).equals("/") && equation.get(i + 1).equals("-")) {
+            double blah = Double.parseDouble(equation.get(i + 2)) * (-1);
+            String blagh = String.valueOf(blah);
+            System.out.println(blah);
+            equation.remove(i + 1);
+            equation.set(i + 1, blagh);
+          }
+        } catch (Exception p) {
+          JOptionPane.showMessageDialog(frame, "invalid input", "Error", JOptionPane.ERROR_MESSAGE);
+          tf.setText("");
+          equation.clear();
+          break;
+        }
 
-        bDiv.addActionListener(e -> tf.setText(tf.getText() + " / "));
+        try {
+          if (equation.get(i).matches("[+\\-\\*\\/]") && equation.get(i + 1).matches("[+\\-\\*\\/]")) {
+            JOptionPane.showMessageDialog(frame, "invalid input", "Error", JOptionPane.ERROR_MESSAGE);
+            tf.setText("");
+            equation.clear();
+            break;
 
-        bClear.addActionListener(e -> tf.setText(""));
+          }
+        } catch (Exception p) {
+          JOptionPane.showMessageDialog(frame, "invalid input", "Error", JOptionPane.ERROR_MESSAGE);
+          tf.setText("");
+          equation.clear();
+          break;
+        }
 
-        del.addActionListener(e -> tf.setText(tf.getText().substring(0, tf.getText().length() - 1)));
+        if (equation.get(i).matches("[+\\-\\*\\/]") && equation.get(i + 1).matches("[+\\-\\*\\/]")) {
+          JOptionPane.showMessageDialog(frame, "invalid input", "Error", JOptionPane.ERROR_MESSAGE);
+          tf.setText("");
+          equation.clear();
+          break;
+        }
 
+        /* loop through whole arraylist
+        if operator is found, do the operation and remove the operator and the two indexes around it */
 
+        if (equation.get(i).equals("+")) {
+          result = Double.parseDouble(equation.get(i - 1)) + Double.parseDouble(equation.get(i + 1));
+          equation.set(i, String.valueOf(result));
+          equation.remove(i - 1);
+          equation.remove(i);
+          i = 0;
 
-        //parse text from field
-        beq.addActionListener(e -> {
-            String text = tf.getText();
+        }
+        if (equation.get(i).equals("-")) {
 
-            String[] splitequation = text.split(" ");
-            if (splitequation[1].equals("-")) {
-                splitequation[0] = "0";
-            }
-            ArrayList < String > equation = new ArrayList < > (Arrays.asList(splitequation));
-            for (int c = 0; c < equation.size(); c++) {
-                if (equation.get(c).isEmpty()) {
-                    equation.remove(c);
-                    System.out.println(equation);
-                }
-            }
+          result = Double.parseDouble(equation.get(i - 1)) - Double.parseDouble(equation.get(i + 1));
+          equation.set(i, String.valueOf(result));
+          equation.remove(i - 1);
+          equation.remove(i);
+          i = 0;
+        }
+        if (equation.get(i).equals("*")) {
+          result = Double.parseDouble(equation.get(i - 1)) * Double.parseDouble(equation.get(i + 1));
+          equation.set(i, String.valueOf(result));
+          equation.remove(i - 1);
+          equation.remove(i);
+          i = 0;
+        }
+        if (equation.get(i).equals("/")) {
+          if (Double.parseDouble(equation.get(i - 1)) == 0 && Double.parseDouble(equation.get(i + 1)) == 0) {
+            JOptionPane.showMessageDialog(frame, "invalid input", "Error", 0);
+          }
+          result = Double.parseDouble(equation.get(i - 1)) / Double.parseDouble(equation.get(i + 1));
+          equation.set(i, String.valueOf(result));
+          equation.remove(i - 1);
+          equation.remove(i);
+          i = 0;
+        }
+        if (equation.size() == 1) {
+          result = Double.parseDouble(equation.get(i));
+        }
+      }
 
-
-            //check for 2 operators in a row
-            for (int i = 0; i < equation.size(); i++) {
-                if(equation.get(i).matches("[+\\-\\*\\/]") && equation.get(i+1).matches("[+\\-\\*\\/]")){
-                    JOptionPane.showMessageDialog(frame, "invalid input", "Error", JOptionPane.ERROR_MESSAGE);
-                    tf.setText("");
-                    equation.clear();
-                    break;
-                }
-
-
-                /* loop through whole arraylist
-                if operator is found, do the operation and remove the operator and the two indexes around it */
-
-                if (equation.get(i).equals("+")) {
-                    result = Double.parseDouble(equation.get(i - 1)) + Double.parseDouble(equation.get(i + 1));
-                    equation.set(i, String.valueOf(result));
-                    equation.remove(i - 1);
-                    equation.remove(i);
-                    i = 0;
-
-                }
-                if (equation.get(i).equals("-")) {
-
-                    result = Double.parseDouble(equation.get(i - 1)) - Double.parseDouble(equation.get(i + 1));
-                    equation.set(i, String.valueOf(result));
-                    equation.remove(i - 1);
-                    equation.remove(i);
-                    i = 0;
-                }
-                if (equation.get(i).equals("*")) {
-                    result = Double.parseDouble(equation.get(i - 1)) * Double.parseDouble(equation.get(i + 1));
-                    equation.set(i, String.valueOf(result));
-                    equation.remove(i - 1);
-                    equation.remove(i);
-                    i = 0;
-                }
-                if (equation.get(i).equals("/")) {
-                    if (Double.parseDouble(equation.get(i - 1)) == 0 && Double.parseDouble(equation.get(i + 1)) == 0) {
-                        JOptionPane.showMessageDialog(frame, "invalid input", "Error", 0);
-                    }
-                    result = Double.parseDouble(equation.get(i - 1)) / Double.parseDouble(equation.get(i + 1));
-                    equation.set(i, String.valueOf(result));
-                    equation.remove(i - 1);
-                    equation.remove(i);
-                    i = 0;
-                }
-                if (equation.size() == 1) {
-                    result = Double.parseDouble(equation.get(i));
-                }
-            }
-
-            if (result % 1 != 0) {
-                tf.setText(text + " = " + result);
-            } else {
-                result = (int) result;
-                tf.setText(text + " = " + (int)result);
-            }
-        });
-    }
+      if (result % 1 != 0) {
+        tf.setText(text + " = " + result);
+      } else {
+        result = (int) result;
+        tf.setText(text + " = " + (int) result);
+      }
+    });
+  }
 }
-
